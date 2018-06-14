5d2850d1527f960591342be0beb30498cda97297
Sean Poh <seanpohyx@gmail.com>
2018-06-14 18:27:05 +0800

    interval timer
    - interval count down timer working (dummy date)
    - schedule now shows live date(by date) schedule

--- src/app/pages/camper/schedule/schedule.component.ts
+++ src/app/pages/camper/schedule/schedule.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ScheduleService } from "../../../service/schedule.service";
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-schedule',
@@ -10,6 +11,9 @@ export class ScheduleComponent implements OnInit {
 	
   	private todayDate;
   	private schedulelist;
+  	private hours;
+  	private mins;
+  	private secs;
 
   constructor(
   	public schService:ScheduleService
@@ -21,25 +25,50 @@ export class ScheduleComponent implements OnInit {
 	  	var date = ["20 JULY", "21 JULY", "22 JULY"];
 
 	  	var scheduleArr = response.values; 
-	  	var schedulelist = [];
 	  	this.schedulelist = this.getSchedule(scheduleArr);
-	  	this.todayDate = date[this.schedulelist[0][1]] + " 2018";
-	  	console.log(scheduleArr);
- 		
+
+		interval(1000).subscribe(n =>
+			var eventDate = new Date(2018, 6, 20, 8, 0);
+		  	var currentTime = new Date();
+
+		  	var countdowntimer = Math.round((eventDate.getTime() - currentTime.getTime())/1000);
+		  	this.hours = Math.floor(countdowntimer/3600);
+		  	this.mins = Math.floor((countdowntimer % 3600)/60);
+		  	this.secs = Math.floor((countdowntimer % 60));
+		);
 
 	})
   }
 
   getSchedule(schedule){
 	  var arrlist = [];
-	  var date = 1;//enter date
+	  var today = new Date().getDate();
+	  var date = (today <= 20) ? 1 : (today <= 21) ? 2 : 3;
+	  this.todayDate = (date == 1) ? "20 JULY 2018" : (date == 2) ? "21 JULY 2018" : "23 JULY 2018";
 
 	  for(var i=0; i < schedule.length; i++){
-		  if(schedule[i][1] == date){
-
-		  	arrlist.push(schedule[i]);
+		  if(schedule[i][1] == date && arrlist[schedule[i][5]-1] == undefined){
+		  	arrlist[schedule[i][5]-1] = schedule[i];
+		  }
+		  else if(schedule[i][1] == date && arrlist[schedule[i][5]-1] != undefined){
+		  	arrlist[schedule[i][5]-1] = schedule[i];
 		  }
 	  }
+
+	  var arrcount = arrlist.length; //because as you splice the count will decrease
+	  var internalcount = 0; //the i varible is not accurate
+
+	  for(var i=0; i<arrcount;i++){
+
+	  	if(arrlist[internalcount] == undefined){
+	  		arrlist.splice(internalcount, 1);
+	  	}
+	  	else{
+	  		internalcount++;
+	  	}
+
+	  }
+
 	  return arrlist;
   }
 
