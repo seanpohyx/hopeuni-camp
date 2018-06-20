66e59296f141a31147bf51ac9f8dd829078152b3
jeremiah-ang <jeremiah@visenze.com>
2018-06-20 16:38:00 +0800

    filter latest entries

--- src/app/service/schedule.service.ts
+++ src/app/service/schedule.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
 import { GooglesheetService } from './googlesheet.service';
+import { Observable } from 'rxjs/Rx';
+import { ExtractTopK } from '../utils/entries.util';
+import { ScheduleEntry } from '../model/ScheduleEntry.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduleService {
 
-	sheetname = "schedule";
+  sheetname = "schedule";
 
-	public getSheet() {
-		return this.googleSheetService.load(this.sheetname)
-	}
+  public getSheet() {
+    return this.googleSheetService.load(this.sheetname)
+  }
+
+  public getLatestEntries(limit: number = 0):Observable<any> {
+    return Observable.create(observer => {
+      this.getSheet().subscribe(response => {
+        const latestEntries = ExtractTopK(response.values, limit, ScheduleEntry.FromSheet)
+        observer.next(latestEntries);
+        observer.complete();
+      })
+    });
+  }
 
   constructor(
-  	public googleSheetService:GooglesheetService
+    public googleSheetService:GooglesheetService
   ) { }
 }
